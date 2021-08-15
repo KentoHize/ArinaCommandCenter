f91692fb84788453f7be929465b60bca130c80a0
日瀬 健人 <kentnewbie@hotmail.com>
2021-08-15 09:39:25 +0900

    Add project files.

---
+++ ArinaCommandCenter/MainForm.Designer.cs
@@ -0,0 +1,76 @@
+﻿
+namespace ArinaCommandCenter
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.btnGameSave = new System.Windows.Forms.Button();
+            this.cbbGameList = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // btnGameSave
+            // 
+            this.btnGameSave.Location = new System.Drawing.Point(277, 38);
+            this.btnGameSave.Name = "btnGameSave";
+            this.btnGameSave.Size = new System.Drawing.Size(125, 42);
+            this.btnGameSave.TabIndex = 0;
+            this.btnGameSave.Text = "遊戲存檔備份";
+            this.btnGameSave.UseVisualStyleBackColor = true;
+            this.btnGameSave.Click += new System.EventHandler(this.btnGameSave_Click);
+            // 
+            // cbbGameList
+            // 
+            this.cbbGameList.FormattingEnabled = true;
+            this.cbbGameList.Location = new System.Drawing.Point(38, 48);
+            this.cbbGameList.Name = "cbbGameList";
+            this.cbbGameList.Size = new System.Drawing.Size(201, 24);
+            this.cbbGameList.TabIndex = 1;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(547, 205);
+            this.Controls.Add(this.cbbGameList);
+            this.Controls.Add(this.btnGameSave);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.Text = "有奈管理面板";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGameSave;
+        private System.Windows.Forms.ComboBox cbbGameList;
+    }
+}
+
---
+++ ArinaCommandCenter/MainForm.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Aritiafel.Characters.Heroes;
+
+namespace ArinaCommandCenter
+{
+    public partial class MainForm : Form
+    {
+
+        private List<GameInfo> GameList = new List<GameInfo>
+        {
+            new GameInfo { Name = "Rance 10", SavePath = @"C:\Users\kentn\OneDrive\ドキュメント", SaveSubDirectory = @"AliceSoft\ランス１０" }
+        };
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            cbbGameList.DataSource = GameList;
+            cbbGameList.DisplayMember = "Name";            
+        }
+
+        private void btnGameSave_Click(object sender, EventArgs e)
+        {
+            if (cbbGameList.SelectedIndex != -1)
+                Sonia.BackupGameSave(((GameInfo)cbbGameList.SelectedItem).SavePath,
+                    ((GameInfo)cbbGameList.SelectedItem).SaveSubDirectory);
+        }
+    }
+}
